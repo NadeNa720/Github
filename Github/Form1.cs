368c08075c2e74dcf73bcfd7955ca0366f84fdb1
denis.jersivs@gmail.com <denis.jersivs@gmail.com>
2020-04-02 20:08:05 +0300

    version 2_0

--- Github/Form1.cs
+++ Github/Form1.cs
@@ -15,6 +15,66 @@ namespace Github
         public Form1()
         {
             InitializeComponent();
+            webBrowser1.ScriptErrorsSuppressed = true;
+            webBrowser1.Width = this.ClientRectangle.Width;
+            webBrowser1.Height = this.ClientRectangle.Height - 50;
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("www.google.com");
+        }
+
+        private void buttonGo_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate(AddressBar.Text);
+        }
+
+        private void buttonBack_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoBack();
+        }
+
+        private void buttonFovard_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoForward();
+        }
+
+        private void buttonRefresh_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Refresh();
+        }
+
+        private void darkToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.FromArgb(50, 50, 50);
+            foreach (Control contr in this.Controls)
+            {
+                contr.BackColor = Color.FromArgb(50, 50, 50);
+                contr.ForeColor = Color.White;
+            }
+        }
+
+        private void toolStripSplitButton1_ButtonClick(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void lightToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.FromArgb(200, 200, 200);
+            foreach (Control contr in this.Controls)
+            {
+                contr.BackColor = Color.FromArgb(200, 200, 200);
+                contr.ForeColor = Color.Black;
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        
     }
 }
